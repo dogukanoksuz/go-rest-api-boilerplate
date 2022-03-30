7f33e40e1a38510caa513c257266226732fc670b
Doğukan Öksüz <me@dogukan.dev>
2022-03-30 18:47:56 +0300

    Fixed initialization issue

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import (
 	"strconv"
 
 	"github.com/dogukanoksuz/go-rest-api-example/pkg/utils"
-	"github.com/dogukanoksuz/go-rest-api-example/platform/database"
 	"github.com/dogukanoksuz/go-rest-api-example/platform/migrations"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	// Application initialization
-	database.Init()
 	migrations.Migrate()
 
 	port, _ := strconv.Atoi(os.Getenv("APP_PORT"))
